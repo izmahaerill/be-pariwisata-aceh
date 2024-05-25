11af1af62aa6228af61cf0b4e02a367eafe65361
izmahaerill <azmik5671@gmail.com>
2024-05-25 10:51:59 +0800

    config: change config cors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import cors from 'cors';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
