ccdd5c1b4f1fdfc54220bbe15f382d43930a81e5
izmahaerill <azmik5671@gmail.com>
2024-05-25 10:55:49 +0800

    config: change config cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   // app.enableCors({
   //   origin: [
   //     'http://localhost:3000',
@@ -14,7 +13,6 @@ async function bootstrap() {
   //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   //   credentials: true,
   // });
-  app.use(cors());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
