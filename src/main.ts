19714332b2eb35bcf24e08336325c01d6a859160
izmahaerill <azmik5671@gmail.com>
2024-05-25 11:07:25 +0800

    config: change config cors again

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
-  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     })
   );
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
