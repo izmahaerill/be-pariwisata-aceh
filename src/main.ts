453c80a9fedd18a8f900b38051f1539ad258e6e0
izmahaerill <azmik5671@gmail.com>
2024-05-25 10:01:59 +0800

    config: change config cors

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:5173',
-      'https://be-pariwisata-aceh.vercel.app',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3000',
+  //     'http://localhost:5173',
+  //     'https://be-pariwisata-aceh.vercel.app',
+  //   ],
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true,
+  // });
+  app.use(cors());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
